1da8a5da3f0a9594d4d6d8f84b409035785cf1ec
Nick Gaswirth <gaswirth@gmail.com>
2017-09-08 15:01:10 -0400

    Fixed HOSTNAME var

--- s3-backup.sh
+++ s3-backup.sh
@@ -5,10 +5,10 @@ HOSTNAME=`hostname`
 
 echo "S3 Backup Started on $HOSTNAME"
 date +'%a %b %e %H:%M:%S %Z %Y'
-s3cmd sync --delete-removed --recursive --preserve --exclude-from $EXCLUDES /home s3://linode-$LINODE
-s3cmd sync --delete-removed --recursive --preserve --exclude-from $EXCLUDES /etc s3://linode-$LINODE
-s3cmd sync --delete-removed --recursive --preserve --exclude-from $EXCLUDES /var s3://linode-$LINODE
+s3cmd sync --delete-removed --recursive --preserve --exclude-from $EXCLUDES /home s3://linode-$HOSTNAME
+s3cmd sync --delete-removed --recursive --preserve --exclude-from $EXCLUDES /etc s3://linode-$HOSTNAME
+s3cmd sync --delete-removed --recursive --preserve --exclude-from $EXCLUDES /var s3://linode-$HOSTNAME
 dpkg --get-selections > "$DIR"/dpkg.list
-s3cmd sync --preserve "$DIR"/dpkg.list s3://linode-$LINODE
+s3cmd sync --preserve "$DIR"/dpkg.list s3://linode-$HOSTNAME
 date +'%a %b %e %H:%M:%S %Z %Y'
 echo 'Finished'
